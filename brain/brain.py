74f8c19ce9800d79e24abab52810a548abbc6e2a
richardvdoost <richard@blendergrid.com>
2020-06-13 17:33:33 +0200

    Add weights regularization functionality

--- brain/brain.py
+++ brain/brain.py
@@ -15,18 +15,21 @@ class Brain:
     represented by a NumPy matrix.
     """
 
-    def __init__(self, topology):
+    def __init__(self, topology, regularization=None):
         """
         Args:
-            topology ((int, TransferFunction|None), ...): Describes each layer of the network with a lenght for the
+            topology ((int, TransferFunction | None), ...): Describes each layer of the network with a lenght for the
                 layer (amount of neurons) and a transfer function object for the neurons in the layer. The input layer
                 can't have a transfer function, so providing None is fine. The output layer should have a ReLU transfer
                 function for weight gradient calculation to work properly with the current cost function.
+            regularization (float | None): Weights regularization to keep weight values from getting too extreme and
+                unstable.
         """
 
         # Create the neuron layers and synapse clusters (connections)
         self.neuron_layers = []
         self.synapse_clusters = []
+        self.__non_bias_weights_count = 0
         for layer_length, activation_function in topology:
 
             self.neuron_layers.append(NeuronLayer(layer_length, activation_function))
@@ -39,10 +42,16 @@ class Brain:
                     self.neuron_layers[-1],
                 )
 
-                self.synapse_clusters.append(SynapseCluster(surrounding_neuron_layers))
+                self.synapse_clusters.append(SynapseCluster(self, surrounding_neuron_layers))
+                self.__non_bias_weights_count += len(surrounding_neuron_layers[0]) * len(surrounding_neuron_layers[1])
 
-        # Initialize the output target (Y)
-        self.target = np.zeros((1, topology[-1][0]))
+        self.__regularization = regularization
+        self.__regularization_factor = regularization
+        self.__batch_size = 1
+        self.target = np.zeros((self.__batch_size, topology[-1][0]))
+
+        # Set some NumPy print options
+        np.set_printoptions(precision=3, suppress=True, floatmode="fixed")
 
     def train(self, training_samples, iteration_count):
         """
@@ -69,11 +78,8 @@ class Brain:
 
             self.optimize_weights()
 
-        # Print the end result
-        print("\ntarget:")
-        print(self.target)
-        print("\noutput:")
-        print(self.output)
+        print(f"\nTarget:\n{self.target}")
+        print(f"\nOutput:\n{self.output}")
 
     def convert_training_samples(self, training_samples):
         """ Convert training data to NumPy matrices and initialize the input (X) and target (Y) of the network """
@@ -81,6 +87,7 @@ class Brain:
         assert len(training_samples[0]["input"]) == len(self.input_layer)
         assert len(training_samples[0]["target"]) == len(self.output_layer)
 
+        self.batch_size = len(training_samples)
         self.input = np.array([sample["input"] for sample in training_samples])
         self.target = np.array([sample["target"] for sample in training_samples])
 
@@ -98,8 +105,22 @@ class Brain:
             synapse_cluster.optimize_weights()
 
     def cost(self):
-        J = -1 * self.target * np.log(self.output) - (1 - self.target) * np.log(1 - self.output)
-        return np.sum(J) / self.target.shape[0]
+        cost_matrix = -1 * self.target * np.log(self.output) - (1 - self.target) * np.log(1 - self.output)
+        cost = np.sum(cost_matrix) / self.batch_size
+
+        if self.regularization_factor is not None:
+            total_weights_squared = 0
+            for synapse_cluster in self.synapse_clusters:
+                total_weights_squared += np.sum(synapse_cluster.weights[:, 1:] ** 2)
+            cost += self.regularization_factor * 0.5 * total_weights_squared
+
+        return cost
+
+    def __str__(self):
+        output = "\nBrain Weights:\n\n"
+        for i, synapse_cluster in enumerate(self.synapse_clusters):
+            output += f"Synapse Cluster {i + 1:02}:\n{synapse_cluster.weights}\n\n"
+        return output
 
     @property
     def input_layer(self):
@@ -120,3 +141,19 @@ class Brain:
     @input.setter
     def input(self, input):
         self.input_layer.output = input
+
+    @property
+    def batch_size(self):
+        return self.__batch_size
+
+    @batch_size.setter
+    def batch_size(self, batch_size):
+        self.__batch_size = batch_size
+
+        # When we set the batch size, update the regularization factor if we use regularization
+        if self.__regularization is not None:
+            self.__regularization_factor = self.__regularization / (self.__batch_size * self.__non_bias_weights_count)
+
+    @property
+    def regularization_factor(self):
+        return self.__regularization_factor
--- brain/synapse_cluster.py
+++ brain/synapse_cluster.py
@@ -6,12 +6,15 @@ class SynapseCluster:
     Describes a set of synapses (connections) between 2 layers of neurons
     """
 
-    def __init__(self, surrounding_neuron_layers):
+    def __init__(self, brain, surrounding_neuron_layers):
         """
         Args:
+            brain (Brain): Reference to the brain this synapse cluster is part of
             surrounding_neuron_layers (NeuronLayer, NeuronLayer): The neuron layers that surround the current synapse
                 cluster on the left and right side respectively.
         """
+
+        self.__brain = brain
 
         # References to neighbours
         self.neurons_left, self.neurons_right = surrounding_neuron_layers
@@ -19,15 +22,25 @@ class SynapseCluster:
 
         weight_matrix_shape = (len(self.neurons_right), len(self.neurons_left) + 1)  # +1 for bias neuron 👍
 
-        self.weights = np.random.random_sample(weight_matrix_shape) - 0.5
+        self.weights = (np.random.random_sample(weight_matrix_shape) - 0.5) * 2
         self.weight_gradients = np.zeros(weight_matrix_shape)
 
     def forward_prop(self):
         self.neurons_right.logit = self.neurons_left.output_with_bias.dot(self.weights.T)
 
     def calculate_gradients(self):
-        m = self.neurons_left.output.shape[0]
-        self.weight_gradients = self.neurons_right.delta.T.dot(self.neurons_left.output_with_bias) / m
+        self.weight_gradients = self.neurons_right.delta.T.dot(self.neurons_left.output_with_bias) / self.batch_size
+
+        if self.regularization_factor is not None:
+            self.weight_gradients[:, 1:] += self.regularization_factor * self.weights[:, 1:]
 
     def optimize_weights(self):
-        self.weights -= self.weight_gradients
+        self.weights -= self.weight_gradients * 0.5
+
+    @property
+    def batch_size(self):
+        return self.__brain.batch_size
+
+    @property
+    def regularization_factor(self):
+        return self.__brain.regularization_factor
